be04a293cfe53f8cd04e40b7381af45ab9d97be7
icaro <icaro.moreira@protonmail.com>
2019-10-29 19:06:46 -0300

    5.0

--- Registrador.v
+++ Registrador.v
@@ -4,11 +4,10 @@ module Registrador(Shift_in, CLK, shift_out, Bit_0, Bit_1, Bit_2, Bit_3, Reset);
     reg Bit_0, Bit_1, Bit_2, Bit_3;
     output wire shift_out;
 
-    assign shift_out = Bit_3;
 
     always @(posedge CLK) begin
         
-        if(Reset == 1'b1) begin
+        if (Reset == 1'b1) begin
             
             Bit_3 <= 1'b0;
             Bit_2 <= 1'b0;
@@ -17,13 +16,15 @@ module Registrador(Shift_in, CLK, shift_out, Bit_0, Bit_1, Bit_2, Bit_3, Reset);
 
         else 
 
-            Bit_3 = Bit_2;
-            Bit_2 = Bit_1;
-            Bit_1 = Bit_0;
-            Bit_0 = Shift_in;
+            Bit_3 = Shift_in;
+            Bit_2 = Bit_3;
+            Bit_1 = Bit_2;
+            Bit_0 = Bit_1
 
         end
 
     end
+
+    assign shift_out = Bit_0;
 
 endmodule 
