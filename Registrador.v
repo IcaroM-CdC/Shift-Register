d1e7ddf186e2ef440231a2f9d8076b197d74b94f
IcaroM-CdC <icaro.moreira@protonmail.com>
2019-10-29 18:17:08 -0300

    v 3.0 Not work

--- Registrador.v
+++ Registrador.v
@@ -1,9 +1,7 @@
 module Registrador(Shift_in, CLK, shift_out, Bit_0, Bit_1, Bit_2, Bit_3, Reset);
 
     input wire Shift_in, CLK, Reset;
-    
     reg Bit_0, Bit_1, Bit_2, Bit_3;
-    
     output wire shift_out;
 
     assign shift_out = Bit_3;
@@ -11,19 +9,21 @@ module Registrador(Shift_in, CLK, shift_out, Bit_0, Bit_1, Bit_2, Bit_3, Reset);
     
     always @(posedge CLK) begin
         
-        if(Reset == 1'b1)
-
+        if(Reset == 1'b1) begin
+            
             Bit_3 <= 1'b0;
             Bit_2 <= 1'b0;
             Bit_1 <= 1'b0;
-            Bit_0 <= 1'b0;  
+            Bit_0 <= 1'b0; 
 
-        else
+        else 
 
-            Bit_3 <= Bit_2;
-            Bit_2 <= Bit_1;
-            Bit_1 <= Bit_0;
-            Bit_0 <= Shift_in;
+            Bit_3 = Bit_2;
+            Bit_2 = Bit_1;
+            Bit_1 = Bit_0;
+            Bit_0 = Shift_in;
+
+        end
 
     end
 
--- testbench.v
+++ testbench.v
@@ -2,14 +2,16 @@
 
 module testbench ();
 
-    reg Shift_in, CLK, Reset;
+    reg Shift_in, CLK, Reset, Bit_0, Bit_1, Bit_2, Bit_3;
     wire shift_out;
 
-    Registrador Reg (.Shift_in(Shift_in), .CLK(CLK), .Reset(Reset), .shift_out(shift_out));
+    Registrador Reg (.Shift_in(Shift_in), .CLK(CLK), .Reset(Reset), .shift_out(shift_out),
+    .Bit_0(Bit_0), .Bit_1(Bit_1), .Bit_2(Bit_2), .Bit_3(Bit_3));
 
 
     initial begin
           
+        $dumpvars;  
         $dumpfile("testbench.vcd");
         $monitor("Shift_in %b Clock %b Bit_0 %b Bit_1 %b Bit_2 %b Bit_3 Shift_out %b");
 
